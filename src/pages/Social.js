1c7e15a5f55bd94b6c92cccb44698c928b0534c5
Mereck600 <160926474+Mereck600@users.noreply.github.com>
2025-08-10 20:02:00 -0500

    Deploy

--- src/pages/Social.js
+++ src/pages/Social.js
@@ -1,87 +1,81 @@
-import React, { useState } from 'react';
-import { Paper, Typography, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, useMediaQuery } from '@mui/material';
+import React from 'react';
+import { Paper, Typography, Button, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { motion } from 'framer-motion';
 
 const Social = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-
-const theme = useTheme();
-const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // Set breakpoint for responsive behavior
-
-return (
-  <Paper
-    elevation={3}
-    style={{
-      padding: '20px',
-      display: 'flex',
-      flexDirection: isMobile ? 'column' : 'row', // Stack on mobile
-      justifyContent: 'space-between',
-      alignItems: isMobile ? 'center' : 'flex-start', // Center items on mobile
-      width: isMobile ? '90%' : '100%',
-      margin: '0 auto', // Center on smaller screens
-      marginBottom:'15px',
-      maxWidth: '800px',
-      gap: isMobile ? '20px' : '0', // Add space between items on mobile
-    }}
-  >
-    <motion.div
-      initial={{ opacity: 0, y: -50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1, delay: 1 }}
+  return (
+    <Paper
+      elevation={3}
       style={{
-        flex: 1,
-        textAlign: 'left',
-        marginRight: isMobile ? '0' : '20px', // Remove margin on mobile
-        marginLeft: isMobile ? '0' : '50px', // Remove margin on mobile
-        width: '100%',
-        textAlign: isMobile ? 'center' : 'left', // Center text on mobile
+        padding: '20px',
+        display: 'flex',
+        flexDirection: isMobile ? 'column' : 'row',
+        justifyContent: 'space-between',
+        alignItems: isMobile ? 'center' : 'flex-start',
+        width: isMobile ? '90%' : '100%',
+        margin: '0 auto',
+        marginBottom: '15px',
+        maxWidth: '800px',
+        gap: isMobile ? '20px' : '0',
       }}
     >
-      <Typography variant="h4" component="h2">Github</Typography>
-      <Typography variant="body1" style={{ marginTop: '10px' }}>
-        View my profile:
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        href ="https://github.com/Mereck600"
-        target ="_blank"
-        style={{ marginTop: '10px' }}
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 1 }}
+        style={{
+          flex: 1,
+          marginRight: isMobile ? '0' : '20px',
+          marginLeft: isMobile ? '0' : '50px',
+          width: '100%',
+          textAlign: isMobile ? 'center' : 'left',
+        }}
       >
-        Github
-      </Button>
-    </motion.div>
+        <Typography variant="h4" component="h2">Github</Typography>
+        <Typography variant="body1" style={{ marginTop: '10px' }}>
+          View my profile:
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          href="https://github.com/Mereck600"
+          target="_blank"
+          style={{ marginTop: '10px' }}
+        >
+          Github
+        </Button>
+      </motion.div>
 
-    <motion.div
-      initial={{ opacity: 0, y: -50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, delay: 0.4 }}
-      style={{
-        flex: 1,
-        textAlign: 'center',
-        width: '100%',
-        marginTop: isMobile ? '10px' : '0',
-      }}
-    >
-      <Typography variant="h4" component="h2">Contact</Typography>
-      <Typography variant="body1" style={{ marginTop: '10px' }}>
-        Feel free to reach out!
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        href="mailto:merecmcg@gmail.com"
-        style={{ marginTop: '10px' }}
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        style={{
+          flex: 1,
+          textAlign: 'center',
+          width: '100%',
+          marginTop: isMobile ? '10px' : '0',
+        }}
       >
-        Email Me
-      </Button>
-    </motion.div>
-
-    
-    
-  </Paper>
-);
+        <Typography variant="h4" component="h2">Contact</Typography>
+        <Typography variant="body1" style={{ marginTop: '10px' }}>
+          Feel free to reach out!
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          href="mailto:merecmcg@gmail.com"
+          style={{ marginTop: '10px' }}
+        >
+          Email Me
+        </Button>
+      </motion.div>
+    </Paper>
+  );
 };
 
 export default Social;
